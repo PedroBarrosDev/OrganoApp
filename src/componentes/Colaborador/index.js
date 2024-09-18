517cecf8ea4ae9b6776192fc09d4b1e5cecfae4a
Pedro Barros <devpedrobarros@hotmail.com>
2024-09-18 15:27:27 -0300

    Card colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/pedrobarrosdev.png' alt='Pedro Barros'/>
+        </div>
+        <div className='rodape'>
+            <h4>Pedro Barros</h4>
+            <h5>DEV</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
