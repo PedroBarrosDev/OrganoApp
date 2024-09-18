931fabf603049f38c5a60675223a98711382daac
Pedro Barros <devpedrobarros@hotmail.com>
2024-09-18 16:17:41 -0300

    Mapeando os colaboradores

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,13 +1,13 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
     return (<div className='colaborador'>
         <div className='cabecalho'>
-            <img src='https://github.com/pedrobarrosdev.png' alt='Pedro Barros'/>
+            <img src={imagem} alt={nome} />
         </div>
         <div className='rodape'>
-            <h4>Pedro Barros</h4>
-            <h5>DEV</h5>
+            <h4>{nome}</h4>
+            <h5>{cargo}</h5>
         </div>
     </div>)
 }
